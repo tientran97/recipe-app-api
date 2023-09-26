05557ef8a15b5d6aec79291a4bf3e5d4cbd298cf
tientran97 <tien.minhtran615034@gmail.com>
2023-09-26 10:54:21 +0700

    again

--- app/app/test.py
+++ app/app/test.py
@@ -11,9 +11,9 @@ class CalcTests(SimpleTestCase):
 
     def test_add_numbers(self):
         """Test adding numbers together."""
-        res = calc.add(5, 6)        
+        res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
