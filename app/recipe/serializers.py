e1604c122fb019eecf122ed5988c8e05b7c9e5c7
tientran97 <tien.minhtran615034@gmail.com>
2023-10-01 18:44:41 +0700

    Write test and implement recipe detail api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -7,9 +7,16 @@ from core.models import Recipe
 
 
 class RecipeSerializer(serializers.ModelSerializer):
-    "Serializer for recipes."
+    """Serializer for recipes."""
 
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
         read_only_fields = ['id']
+
+
+class RecipeDetailSerializer(serializers.ModelSerializer):
+    """Serializer for recipe detail view."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
